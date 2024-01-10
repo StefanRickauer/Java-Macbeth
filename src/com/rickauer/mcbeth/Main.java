ef47949c0e2c312d782b6184e7a6ff76528a9e5d
Stefan Rickauer <stefanrick@gmx.de>
2024-01-10 19:49:22 +0100

    Specified purpose of global try-catch-block

--- src/com/rickauer/mcbeth/Main.java
+++ src/com/rickauer/mcbeth/Main.java
@@ -3,10 +3,11 @@ package com.rickauer.mcbeth;
 public class Main {
 	public static void main(String[] args) {
 		
-		// Fehlerbehandlung und Kontextbildung
+		// Global try-catch-block for error handling. All other methods throw RuntimeExceptions in order to be processed here. 
 		try {
 			System.out.println("McBeth-Compiler alive and kicking.");
 		} catch (Exception e) {
+			System.err.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
