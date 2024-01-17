0c4c5e22e01ed77ca83776fd36355fe5ddc53a66
Stefan Rickauer <stefanrick@gmx.de>
2024-01-17 20:07:24 +0100

    Revised grammar, added logic to Scanner and Token classes.

--- src/com/rickauer/mcbeth/syntacticAnalyzer/Scanner.java
+++ src/com/rickauer/mcbeth/syntacticAnalyzer/Scanner.java
@@ -1,5 +1,58 @@
 package com.rickauer.mcbeth.syntacticAnalyzer;
 
 public class Scanner {
-	// TODO implement class
+	private char currentChar;		// first source character
+	
+	// Kind and spelling of the current token
+	private byte currentKind;
+	private StringBuffer currentSpelling;
+	
+	private void take(char exprectedChar) {
+		if (currentChar == exprectedChar) {
+			currentSpelling.append(currentChar);
+			//currentChar =  next source character ;
+		} else {
+			// report lexical error
+		}
+	}
+	
+	private void takeIt() {
+		currentSpelling.append(currentChar);
+		//currentChar = next source character;
+	}
+	
+	private boolean isDigit(char c) {
+		// returns true if character is digit
+		return false; 	// dummy value
+	}
+	
+	private boolean isLetter(char c) {
+		// returns true if character is letter
+		return false; 	// dummy value
+	}
+	
+	private boolean isWhitespace(char c) {
+		// returns true if character is white space
+		return false; 	// dummy value
+	}
+	
+	@SuppressWarnings("removal")
+	private byte scanToken() {
+		// as above
+		return new Byte(currentKind); // dummy value
+	}
+	
+	private void scanSeparator() {
+		// as above
+	}
+	
+	public Token scan() {
+		while (currentChar == '!' || currentChar == ' ' || currentChar == '\n') {
+			scanSeparator();
+		}
+		
+		currentSpelling = new StringBuffer("");
+		currentKind = scanToken();
+		return new Token(currentKind, currentSpelling.toString());
+	}
 }
--- src/com/rickauer/mcbeth/syntacticAnalyzer/Token.java
+++ src/com/rickauer/mcbeth/syntacticAnalyzer/Token.java
@@ -1,5 +1,24 @@
 package com.rickauer.mcbeth.syntacticAnalyzer;
 
 public class Token {
-	// TODO implement class
+	public byte kind;
+	public String spelling;
+	
+	public Token(byte kind, String spelling) {
+		this.kind = kind;
+		this.spelling = spelling;
+		
+		if (kind == IDENTIFIER) {
+			for (int i = NUMBER; i <= SHOW; i++ ) {
+				if (spelling.equals(spellings[i])) {
+					this.kind = (byte)i;
+					break;
+				}
+			}
+		}
+	}
+	
+	public final static byte IDENTIFIER = 0, INTLITERAL = 1, OPERATOR = 2, NUMBER = 3, STRING = 4, SHOW = 5, DOT = 6, BECOMES = 7, LBRACKET = 8, RBRACKET = 9, EOT = 10;
+	
+	private final static String[] spellings = { "<identifier>", "<integer-literal>", "<operator>", "number", "string", "show", ".", "<-", "[", "]", "<eot>" }; 
 }
