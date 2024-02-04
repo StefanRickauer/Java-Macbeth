fd07af425046e30c0d2b64bbe20d13be694df49a
Stefan Rickauer <stefanrick@gmx.de>
2024-02-04 13:47:46 +0100

    Update

--- src/com/rickauer/macbeth/abstractsyntaxtrees/ActualParameter.java
+++ src/com/rickauer/macbeth/abstractsyntaxtrees/ActualParameter.java
@@ -2,6 +2,8 @@ package com.rickauer.macbeth.abstractsyntaxtrees;
 
 import com.rickauer.macbeth.syntacticAnalyzer.SourcePosition;
 
+// test comment
+
 public abstract class ActualParameter extends AST {
 	
 	public ActualParameter(SourcePosition position) {
