653180c0e0288762a89aef39e4b3d88986b7fc4b
Stefan Rickauer <stefanrick@gmx.de>
2024-02-11 14:28:06 +0100

    Refactored integration test while trying to focus despite carneval being in full swing

--- integration-test/com/rickauer/macbeth/IntegrationTestMain.java
+++ integration-test/com/rickauer/macbeth/IntegrationTestMain.java
@@ -1,5 +1,6 @@
 package com.rickauer.macbeth;
 
+
 import com.rickauer.macbeth.abstractsyntaxtrees.Program;
 import com.rickauer.macbeth.syntacticAnalyzer.Parser;
 import com.rickauer.macbeth.syntacticAnalyzer.Scanner;
@@ -8,18 +9,27 @@ import com.rickauer.macbeth.syntacticAnalyzer.SourceFile;
 public class IntegrationTestMain {
 	
 	public static void main(String[] args) {
-		String currentDirectory = System.getProperty("user.dir");
-		Scanner scanner = new Scanner(new SourceFile(currentDirectory + "/resources/test data/test-tokensWithKeywords"));
-		Parser parser = new Parser(scanner);
-		Program program;
 		
 		try {
 			System.out.println("Starting integration test...");
-			program = parser.parseProgram();
-			System.out.println(program.command.getClass());
+			
+			if (sequentialCommandTest().contains("SequentialCommand")) {
+				System.out.println("Successfully parsed sequential commands.");
+			} else {
+				System.out.println("Error parsing sequential commands.");
+			}
+			
 			System.out.println("Finished integration test.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static String sequentialCommandTest() {
+		String currentDirectory = System.getProperty("user.dir");
+		Scanner scanner = new Scanner(new SourceFile(currentDirectory + "/resources/test data/test-tokensWithKeywords"));
+		Parser parser = new Parser(scanner);
+		Program program = parser.parseProgram();
+		return program.command.getClass().toString();
 	}
 }
