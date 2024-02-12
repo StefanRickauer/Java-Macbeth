d6a734c39552872cf6050d486fb09ae70478b1d7
Stefan Rickauer <stefanrick@gmx.de>
2024-02-12 19:29:54 +0100

    Added logic to Compiler class

--- src/com/rickauer/macbeth/Macbeth.java
+++ src/com/rickauer/macbeth/Macbeth.java
@@ -1,13 +1,46 @@
 package com.rickauer.macbeth;
 
+import com.rickauer.macbeth.abstractsyntaxtrees.Program;
+import com.rickauer.macbeth.semanticAnalyzer.Checker;
+import com.rickauer.macbeth.syntacticAnalyzer.Parser;
+import com.rickauer.macbeth.syntacticAnalyzer.Scanner;
+
 public class Macbeth {
+	
+	private static String objectName = "obj.beth";
+	
+	private static Scanner scanner;
+	private static Parser parser;
+	private static Checker checker;
+//	private static Encoder encoder;
+	private static ErrorReporter errorReporter;
+	
+	private static Program programAST;
+	
 	public static void main(String[] args) {
 		
+		if(args.length != 1) {
+			System.out.println("Usage: macbeth <source file>");
+			System.exit(1);
+		}
+		
+		String sourceFile = args[0];
+		boolean compiledSuccessfully;
+		
 		try {
-			System.out.println("McBeth-Compiler alive and kicking.");
+			compileProgram(sourceFile, objectName, false, false);
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 			e.printStackTrace();
 		}
+	}
+	
+	private static boolean compileProgram(String source, String object, boolean showAST, boolean showTable) {
+		System.out.println("Macbeth Compiler - Version: 0.2\n" +
+				"===============================\n\n" +
+				"There's no art\n"
+				+ "To find the mind's construction in the face.");
+		
+		// TODO: Implement remaining logic
 	}
 }
